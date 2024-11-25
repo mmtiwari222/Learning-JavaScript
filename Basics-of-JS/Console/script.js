83ffed19760bcb76b445977c6fcb3fdd60158b40
Madanmohan Tiwari <mmtiwari222@gmail.com>
2024-11-25 17:26:57 +0530

    Some Methods of Console

---
+++ Basics-of-JS/Console/script.js
@@ -0,0 +1,24 @@
+// Some methods of console object
+
+// print anything on console
+console.log("Print Anything");
+
+// for showing any error on console
+console.error("Here is Error");
+
+// for showing any warning on console
+console.warn("Here is Warning");
+
+// for print anything in a form of table on console
+console.table({ name: "Madanmohan", age: 22 });
+
+// group something on console
+console.group("A Simple Group");
+console.log("Print Anything");
+console.error("Here is Error");
+console.warn("Here is Warning");
+console.groupEnd();
+
+// try to use css styles on direct console elements
+const styles = "padding: 10px; background-color: white; color: black;";
+console.log("%cHello World!", styles);
