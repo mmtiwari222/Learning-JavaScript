e5c7b64eb05c0a944fb8783928ca6bba2b740ce6
Madanmohan Tiwari <mmtiwari222@gmail.com>
2025-01-27 16:04:19 +0530

    Operators in JavaScript

---
+++ Basics-of-JS/Operators/script.js
@@ -0,0 +1,88 @@
+// Operators in JavaScript
+
+// Operators are used to perform operations on variables and values. JavaScript has the following types of operators:
+
+// Arithmetic operators
+// let score = 10;
+// score = score + 30; // 40 //addition
+// score = score - 5; // 35 //subtraction
+// score = score * 2; // 70 //multiplication
+// score = score / 2; // 35 //division
+// score = score % 2; // 1 //modulus
+// score = score ** 2; // 1 //exponentiation
+// score = score++; // 1 //increment
+// score = ++score; // 2 //increment
+// score = score--; // 2 //decrement
+// score = --score; // 1 //decrement
+
+// console.log(score);
+
+// increment and decrement operators
+// pre-increment operator
+// let x = 5;
+// let y = ++x; // x = 6, y = 6
+// console.log(x, y);
+
+// post-increment operator
+// let a = 5;
+// let b = a++; // a = 6, b = 5
+// console.log(a, b);
+
+// pre-decrement operator
+// let m = 5;
+// let n = --m; // m = 4, n = 4
+// console.log(m, n);
+
+// post-decrement operator
+// let p = 5;
+// let q = p--; // p = 4, q = 5
+// console.log(p, q);
+
+// Assignment operators
+// let a = 10; // 10   // assignment
+// a += 5; // 15 // addition
+// a -= 5; // 10  // subtraction
+// a *= 2; // 20 // multiplication
+// a /= 2; // 10 // division
+// a %= 3; // 1 // modulus
+// a **= 2; // 1 // exponentiation
+// console.log(a);
+
+// Comparison operators
+// let x = 5;
+// let y = 10;
+// let z = "5";
+// console.log(x == z); // true // equal to
+// console.log(x === z); // false // equal value and equal type
+// console.log(x != y); // true // not equal
+// console.log(x !== z); // true // not equal value or not equal type
+// console.log(x > y); // false // greater than
+// console.log(x < y); // true // less than
+// console.log(x >= y); // false // greater than or equal to
+// console.log(x <= y); // true // less than or equal to
+
+// Logical operators
+// let p = 5;
+// let q = 10;
+// console.log(p < 10 && q > 5); // true // and
+// console.log(p < 10 || q < 5); // true // or
+// console.log(!(p < 10)); // false // not
+
+// String operators // concatenation
+// let firstName = "Madanmohan";
+// let lastName = "Tiwari";
+// console.log(firstName + " " + lastName); // Madanmohan Tiwari // concatenation
+
+// Conditional (ternary) operator
+// let age = 20;
+// let type = age > 18 ? "Adult" : "Child";
+// console.log(type); // Adult
+
+// Unary operators
+// let x = 5;
+// console.log(-x); // -5 // negation
+// console.log(+x); // 5 // plus
+// console.log(++x); // 6 // increment
+// console.log(--x); // 5 // decrement
+// console.log(~x); // -6 // bitwise NOT
+// console.log(!x); // false // logical
