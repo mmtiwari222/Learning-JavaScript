d2b88fc0fb9ee4393a26953a70d66311d613f47e
Madanmohan Tiwari <mmtiwari222@gmail.com>
2026-09-01 06:37:04 +0000

    feat: add basic operators and template string practice

--- New JS Journey/Practice Assignments/valuesVariables.js
+++ New JS Journey/Practice Assignments/valuesVariables.js
@@ -1,24 +1,31 @@
-// Declare country as a constant (cannot be changed after initialization)
+// Basic data variables
 const country = "INDIA";
-
-// Declare continent as a constant
 const continent = "Asia";
-
-// Declare population as a variable using let (can be reassigned)
 let population = 1500000000;
-
-// Declare isIsLand as a constant with boolean value
 const isIsLand = false;
-// Attempting to reassign a const variable will throw TypeError
-// isIsLand = true; - TypeError: Assignment to constant variable.
-
-// Declare language as a variable using let
 let language = "Hindi";
-// Reassign language to a new value (let allows reassignment)
 language = "English";
 
-// Print all variables to the console
+// Print values and data types
 console.log(country, continent, population, language);
+console.log(typeof(isIsLand), typeof(population), typeof(country), typeof(language));
 
-// Print the data types of each variable using the typeof operator
-console.log(typeof(isIsLand), typeof(population), typeof(country), typeof(language))
+// Practice arithmetic and comparison operators
+const halfPopulation = population / 2;
+console.log(halfPopulation);
+
+const newPopulation = population++ ;
+console.log(newPopulation);
+
+const finlandPopulation = 6000000;
+console.log(population > finlandPopulation);
+
+const averagePopulation = 33000000;
+console.log(population < averagePopulation);
+
+// String concatenation and template literals
+const description = country + " is in " + continent + ", and its " + population + " people speak " + language;
+console.log(description);
+
+const descriptionTemplate = `${country} is in ${continent}, and its ${population} people speak ${language}`;
+console.log(descriptionTemplate);
